31641e9c5ed7c9a4429c7660d0ccee9c7ad1dcfa
Rinaldi <rinaldiamfine@gmail.com>
2023-05-11 15:03:54 +0700

    Add list component and update the global app css

--- frontend/components/List.tsx
+++ frontend/components/List.tsx
@@ -2,22 +2,35 @@ import React from "react"
 
 export type ListModel = {
     id: string,
+    subject: string,
     name: string,
-    email: string
+    email: string,
+    imageURL: any,
+    status_id: string,
+    status_name: string
 }
 
-export function List(datas: ListModel[]) {
+export function List(datas: ListModel[], isUseImage: boolean) {
     return (
         <ul role="list" className="divide-y divide-gray-200">
             {datas.map((data) => (
                 <li key={data.id} className="flex justify-between gap-x-6 py-5">
                     <div className="flex gap-x-4">
-                        {/* <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={data.imageUrl} alt="" /> */}
-                        {/* <div className="min-w-0 flex-auto"> */}
-                        <p className="text-sm font-semibold leading-6 text-gray-900">{data.name}</p>
-                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">{data.email}</p>
-                        {/* </div> */}
+                        { isUseImage == true ? (
+                         data.imageURL != null ? (
+                            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={data.imageURL} alt="" /> ) : <div className="h-12 w-12 flex-none rounded-full bg-white"/>
+                        ) : "" }
+                        <div className="min-w-0 flex-auto">
+                            <p className="text-sm font-semibold leading-6 text-gray-900">{data.name}</p>
+                            <p className="mt-1 truncate text-xs leading-5 text-gray-500">{data.email}</p>
+                        </div>
                     </div>
+                    <div className="flex gap-x-4">
+                        <div className="min-w-0 flex-auto">
+                            <p className="text-sm font-semibold leading-6 text-gray-900">{data.subject}</p>
+                        </div>
+                    </div>
+
                     {/* <div className="hidden sm:flex sm:flex-col sm:items-end">
                         <p className="text-sm leading-6 text-gray-900">{data.role}</p>
                         {data.lastSeen ? (
--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -6,16 +6,19 @@ const people: ListModel[] = [
     id: '1',
     name: 'Leslie Alexander',
     email: 'leslie.alexander@example.com',
+    imageURL: null
   },
   {
     id: '2',
     name: 'Michael Foster',
     email: 'michael.foster@example.com',
+    imageURL: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
   },
   {
     id: '3',
     name: 'Dries Vincent',
     email: 'dries.vincent@example.com',
+    imageURL: null
   }
 ]
 
@@ -29,7 +32,7 @@ export default function Dashboard() {
       </header>
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          { List(people) }
+          { List(people, true) }
         </div>
       </main>
     </InternalLayout>
